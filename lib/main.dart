b14ee934c3a6fd2b13f2c0a9c14ce5fbfc9db932
Maksim Romanov <maksim.romanov@jetrockets.pro>
2021-12-25 15:59:38 +0300

    Initia; setup application

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,122 +11,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'second Startup Name Generator',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.white,
-          foregroundColor: Colors.black,
+        textTheme: GoogleFonts.nunitoSansTextTheme(
+          Theme.of(context).textTheme,
         ),
       ),
-      home: const RandomWords(),
-    );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  const RandomWords({Key? key}) : super(key: key);
-
-  @override
-  _RandomWordsState createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-  final _saved = <WordPair>{};
-
-  @override
-  Widget build(BuildContext context) {
-    void _pushSaved() {
-      Navigator.of(context).push(
-        MaterialPageRoute<void>(
-          builder: (context) {
-            final tiles = _saved.map((savedPair) {
-              return ListTile(
-                title: Text(
-                  savedPair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            });
-
-            List<Widget> _getDivided(Iterable<ListTile> tileProps) {
-              if (!tileProps.isNotEmpty) return [];
-
-              return ListTile.divideTiles(
-                tiles: tileProps,
-                context: context,
-              ).toList();
-            }
-
-            return Scaffold(
-              appBar: AppBar(
-                title: const Text('Saved suggestions'),
-              ),
-              body: ListView(children: _getDivided(tiles)),
-            );
-          },
-        ),
-      );
-    }
-
-    ;
-
-    // final wordPair = WordPair.random();
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('first Startu name generator'),
-        actions: [
-          IconButton(
-            onPressed: _pushSaved,
-            icon: const Icon(Icons.list),
-            tooltip: 'Saved suggestions tooltip',
+      home: const Scaffold(
+        body: Center(
+          child: Text(
+            'Hello World!',
+            style: TextStyle(
+              fontSize: 22.0,
+              // fontWeight: FontWeight.bold,
+            ),
           ),
-        ],
+        ),
       ),
-      body: _buildSuggestions(),
-    );
-  }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-      itemBuilder: (context, i) {
-        if (i.isOdd) return const Divider();
-
-        final index = i ~/ 2;
-
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-
-        return _buildRow(_suggestions[index]);
-      },
-    );
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
-
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : Colors.purple.withGreen(100),
-        semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-            return;
-          }
-
-          _saved.add(pair);
-        });
-      },
     );
   }
 }
